7882b145ad55b1f29f103864d2273378b1e14140
Sigge Stjärnholm <sigge.stjarnholm@gmail.com>
2015-07-05 20:44:28 +0200

    Added updated Overview tab, read/write settings, 16-1024px icons

--- SFML-test/main.cpp
+++ SFML-test/main.cpp
@@ -1,6 +1,11 @@
 #include "stdafx.h"
 #include <iostream>
+#include <fstream>
 #include <SFML/Graphics.hpp>
+#include <sstream> 
+#include <string>
+
+
 
 using namespace std;
 
@@ -14,17 +19,41 @@ void menuLightning();
 void menuInstructions();
 void menuOptions();
 void menuCredits();
+void getValue();
+void setValue(int com, int baud);
+template <typename T>
+T StringToNumber(const string &Text, T defValue);
 
+int portCOM;
+int baudRate;
 int currentMenu = 0; //0: Overview, 1: Lightning, 2: Instructions, 3: Options: 4: Credits
+
+sf::Sprite logo256Sprite;
 
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(800, 500), "RGB Strip Color Picker");
 	window.setFramerateLimit(60);
+	
+	sf::Image icon;
+	if (!icon.loadFromFile("logo256.png"))
+	{
+		cout << "Error loading 'logo256.png' for icon.";
+	}
+	window.setIcon(256, 256, icon.getPixelsPtr());
+
+	sf::Texture logo256;
+	if (!logo256.loadFromFile("logo256.png"))
+	{
+		cout << "Error loading 'logo256.png'.";
+	}
+	
+	logo256Sprite.setTexture(logo256);
+	logo256Sprite.setPosition(sf::Vector2f(662, 10));
 
 	sf::Font font;
 	if (!font.loadFromFile("Roboto-Light.ttf")){
-		cout << "Error loading font" << endl;
+		cout << "Error loading font 'Roboto-Light'." << endl;
 	}
 
 	sf::Text textMenu[5];
@@ -49,6 +78,10 @@ int main()
 	sf::Text textOverview;
 
 	textOverview.setFont(font);
+	textOverview.setColor(sf::Color::Black);
+	textOverview.setPosition(210, 10);
+	textOverview.setCharacterSize(20);
+	textOverview.setString("Thank you for using RGB Strip Color Picker! \nThis is a tool which you can use to modify your \nRGB color strip attached to an Arduino. Before you \nstart, make sure to read the instructions and check \nout the GitHub page for more information on how \nyou should use this tool. \n\nMore functions and modes will be added by request, so head over \nto the 'Credits' tab and see how you can give feedback and \ncontribute to the project.");
 
 	line[0].position = sf::Vector2f(200, 10);
 	line[1].position = sf::Vector2f(200, 490);
@@ -68,9 +101,6 @@ int main()
 	for (int i = 0; i < 20; i++){
 		menuFade[i].color = sf::Color::White;
 	}
-
-	/*menuFade[1].color = sf::Color(0, 170, 255);
-	menuFade[2].color = sf::Color(0, 170, 255);*/
 
 	menuFade[0].position = sf::Vector2f(10, 10);
 	menuFade[1].position = sf::Vector2f(200, 10);
@@ -101,8 +131,15 @@ int main()
 
 	menuOverview();
 
+	setValue(3, 9600);
+
+	getValue();
+
+	cout << "COM: " + to_string(portCOM) + " Baud: " + to_string(baudRate) << endl;
+
 	while (window.isOpen())
 	{
+
 		mousePos = sf::Mouse::getPosition(window);
 
 		sf::Event event;
@@ -113,18 +150,23 @@ int main()
 			if (sf::Mouse::isButtonPressed(sf::Mouse::Left)){
 				
 				if (mousePos.x >= 10 && mousePos.x <= 200 && mousePos.y >= 10 && mousePos.y <= 100){
+					currentMenu = 0;
 					menuOverview();
 				}
 				else if (mousePos.x >= 10 && mousePos.x <= 200 && mousePos.y >= 100 && mousePos.y <= 200){
+					currentMenu = 1;
 					menuLightning();
 				}
 				else if (mousePos.x >= 10 && mousePos.x <= 200 && mousePos.y >= 200 && mousePos.y <= 300){
+					currentMenu = 2;
 					menuInstructions();
 				}
 				else if (mousePos.x >= 10 && mousePos.x <= 200 && mousePos.y >= 300 && mousePos.y <= 400){
+					currentMenu = 3;
 					menuOptions();
 				}
 				else if (mousePos.x >= 10 && mousePos.x <= 200 && mousePos.y >= 400 && mousePos.y <= 490){
+					currentMenu = 4;
 					menuCredits();
 				}
 			}
@@ -138,22 +180,24 @@ int main()
 			window.draw(textMenu[i]);
 		}
 
-		if (currentMenu = 0){
+		if (currentMenu == 0){
 		//Overview
+			window.draw(textOverview);
+			window.draw(logo256Sprite);
 
-		}else if (currentMenu = 1){
+		}else if (currentMenu == 1){
 		//Lightning
 
 		}
-		else if (currentMenu = 2){
+		else if (currentMenu == 2){
 		//Instructions
 
 		}
-		else if (currentMenu = 3){
+		else if (currentMenu == 3){
 		//Options
 
 		}
-		else if (currentMenu = 4){
+		else if (currentMenu == 4){
 		//Credits
 
 		}
@@ -166,8 +210,6 @@ int main()
 
 void menuOverview(){
 
-	currentMenu = 0;
-
 	for (int i = 0; i < 20; i++){
 		menuFade[i].color = sf::Color::White;
 	}
@@ -175,11 +217,11 @@ void menuOverview(){
 	menuFade[1].color = sf::Color(0, 170, 255);
 	menuFade[2].color = sf::Color(0, 170, 255);
 
+	logo256Sprite.setScale(sf::Vector2f(0.5, 0.5));
+
 }
 
 void menuLightning(){
-
-	currentMenu = 1;
 
 	for (int i = 0; i < 20; i++){
 		menuFade[i].color = sf::Color::White;
@@ -192,8 +234,6 @@ void menuLightning(){
 
 void menuInstructions(){
 
-	currentMenu = 2;
-
 	for (int i = 0; i < 20; i++){
 		menuFade[i].color = sf::Color::White;
 	}
@@ -204,8 +244,6 @@ void menuInstructions(){
 }
 
 void menuOptions(){
-
-	currentMenu = 3;
 
 	for (int i = 0; i < 20; i++){
 		menuFade[i].color = sf::Color::White;
@@ -218,8 +256,6 @@ void menuOptions(){
 
 void menuCredits(){
 
-	currentMenu = 4;
-
 	for (int i = 0; i < 20; i++){
 		menuFade[i].color = sf::Color::White;
 	}
@@ -227,4 +263,49 @@ void menuCredits(){
 	menuFade[17].color = sf::Color(0, 170, 255);
 	menuFade[18].color = sf::Color(0, 170, 255);
 
+}
+
+void getValue(){
+	
+	string com;
+	string baud;
+	int resultCom;
+	int resultBaud;
+	ifstream settings("options.txt");
+	if (settings.is_open()){
+		getline(settings, com);
+		getline(settings, baud);
+		settings.close();
+
+		portCOM = StringToNumber(com, 0.0);
+		baudRate = StringToNumber(baud, 0.0);
+
+	}
+	else{
+		cout << "Unable to open file 'options.txt'." << endl;
+	}
+}
+
+void setValue(int com, int baud){
+
+	ofstream options("options.txt");
+	if (options.is_open()){
+		options << com << endl;
+		options << baud << endl;
+		options.close();
+	}
+	else{
+		cout << "Unable to open file 'options.txt'." << endl;
+	}
+}
+
+template <typename T>
+T StringToNumber(const string &Text, T defValue = T())
+{
+	stringstream ss;
+	for (string::const_iterator i = Text.begin(); i != Text.end(); ++i)
+		if (isdigit(*i) || *i == 'e' || *i == '-' || *i == '+' || *i == '.')
+			ss << *i;
+	T result;
+	return ss >> result ? result : defValue;
 }
